a74ab2106f4ecda9e1b705edf2a5be6cfd6fb6f3
teknosuper <ponyow@gmail.com>
2020-12-12 13:24:05 +0700

    commit show error

--- web/index.php
+++ web/index.php
@@ -1,9 +1,15 @@
 <?php
 
-// comment out the following two lines when deployed to production
 ini_set('date.timezone', 'Asia/Jakarta');
+
+error_reporting($errorlevel & ~(E_NOTICE|E_WARNING));
+// comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
+
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', 300 );
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
