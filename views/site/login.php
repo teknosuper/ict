50aa7308c1ec6f4283cc2647b5426f4301748416
teknosuper <ponyow@gmail.com>
2020-12-12 21:04:16 +0700

    testing login

--- views/site/login.php
+++ views/site/login.php
@@ -15,31 +15,36 @@ $fieldOptions2 = [
 ];
 ?>
 
-    <div class="blog-login col-md-4 col-xs-12 col-sm-12 col-md-offset-4">
-        <div class="blog-login-in" style="width: 100%;">
-            <?php $form = ActiveForm::begin(['id' => 'login-form', 'enableClientValidation' => false]); ?>
-                <img src="/templates/maindashboard/images/logo.png" alt="" />
-                <div class="row">
-                    <?= $form
-                        ->field($model, 'username', $fieldOptions1)
-                        ->label(false)
-                        ->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
-                </div>
-                <div class="row">
-                    <?= $form
-                        ->field($model, 'password', $fieldOptions2)
-                        ->label(false)
-                        ->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
-                </div>
-                <div class="row">
-                        <?= Html::submitButton('Masuk Sistem', ['class' => 'waves-effect waves-light btn-large btn-log-in btn-block', 'name' => 'login-button']) ?>
-                </div>
-                <div class="row">
-                    <div class="col-md-12 text-center">
-                        <span style="color:#e03a74;"><b><?= \app\models\HostnameClass::getHost();?> &copy; <?= date('Y');?></span>                                            
-                    </div>
-                </div>
-                <!-- <a href="forgot.html" class="for-pass">Forgot Password?</a> -->
-            <?php ActiveForm::end(); ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="well">
+                    <?php $form = ActiveForm::begin(['id' => 'login-form', 'enableClientValidation' => false]); ?>
+                        <img src="/templates/maindashboard/images/logo.png" alt="" />
+                        <div class="row">
+                            <?= $form
+                                ->field($model, 'username', $fieldOptions1)
+                                ->label(false)
+                                ->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
+                        </div>
+                        <div class="row">
+                            <?= $form
+                                ->field($model, 'password', $fieldOptions2)
+                                ->label(false)
+                                ->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
+                        </div>
+                        <div class="row">
+                                <?= Html::submitButton('Masuk Sistem', ['class' => 'waves-effect waves-light btn-large btn-log-in btn-block', 'name' => 'login-button']) ?>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 text-center">
+                                <span style="color:#e03a74;"><b><?= \app\models\HostnameClass::getHost();?> &copy; <?= date('Y');?></span>                                            
+                            </div>
+                        </div>
+                        <!-- <a href="forgot.html" class="for-pass">Forgot Password?</a> -->
+                    <?php ActiveForm::end(); ?>
+                </div>                            
+            </div>
+
         </div>
     </div>
